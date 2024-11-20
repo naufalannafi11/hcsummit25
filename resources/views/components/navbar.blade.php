aa80b9a67fb09f14d7613a831546535f2e4b8699
naufalannafi11 <naufalannafi201@gmail.com>
2024-11-20 16:01:16 +0700

    final

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="bg-yellow-500" x-data="{ isOpen: false }">
+<nav class="bg-yellow-500 fixed top-0 left-0 right-0 z-50" x-data="{ isOpen: false }">
     <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
       <div class="flex h-16 items-center justify-between">
         <div class="flex items-center">
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -7,7 +7,7 @@
                 <!-- Left Column: Google Maps Embed -->
             <div class="w-full md:w-2/3">
                 <iframe class="w-full h-96 rounded-lg shadow-lg"
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d126915.48842032172!2d106.71046977796825!3d-6.205286650660918!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f3e36d56aeb7%3A0x8bb60e99b7c542ed!2sKantor%20Pusat%20Perum%20BULOG!5e0!3m2!1sen!2sid!4v1695060708725!5m2!1sen!2sid"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.373994293588!2d106.80463057480313!3d-6.21430889377363!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f6adbd77af01%3A0x23abed373d7987d2!2sJakarta%20Convention%20Center!5e0!3m2!1sen!2sid!4v1732085738667!5m2!1sen!2sid"
                 allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
     
@@ -18,8 +18,8 @@
                     <p class="font-bold mt-7">Day/Date</p>
                     <p>Thursday, September 26th  2024</p>
                     <p class="font-bold mt-7">Vanue</p>
-                    <p>Gedung Widjajono Partowidagdo BPSDM ESDM</p>
-                    <p>Address : Jl. Gatot Subroto No. Kav. 49 Jakarta Selatan</p>
+                    <p>Jakarta Convention Center, Jakarta</p>
+                    <p>Jl. Gatot Subroto No.1, RT.1/RW.3, Gelora, Kecamatan Tanah Abang, Kota Jakarta Pusat, Daerah Khusus Ibukota Jakarta 10270</p>
                     <h2 class="font-bold text-xl underline underline-offset-8 mt-7">CONTACT US</h2> 
                         
                     <div class="mb-4 mt-2">
